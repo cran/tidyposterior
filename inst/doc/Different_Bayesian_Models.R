b73c1d960d7d3636ce9142a19164eea09a6cbaa0
Max Kuhn <max@rstudio.com>
2020-06-11 16:30:02 +0000

    version 0.0.3

--- inst/doc/Different_Bayesian_Models.R
+++ inst/doc/Different_Bayesian_Models.R
@@ -1,4 +1,4 @@
-## ----check-for-build, echo = FALSE, results='asis'-----------------------
+## ----check-for-build, echo = FALSE, results='asis'----------------------------
 eval_chunks <- as.logical(Sys.getenv("local_vignette_build", FALSE))
 # Change this via `Sys.setenv(local_vignette_build = "TRUE")`
 if(!eval_chunks) 
@@ -15,7 +15,7 @@ if(!eval_chunks)
 #  theme_set(theme_bw())
 #  options(width = 100, digits = 3)
 
-## ----rmse-results, eval = eval_chunks------------------------------------
+## ----rmse-results, eval = eval_chunks-----------------------------------------
 #  library(tidyposterior)
 #  data("noisy_example")
 #  
@@ -25,7 +25,9 @@ if(!eval_chunks)
 #     select(id, id2, contains("RMSE")) %>%
 #     setNames(tolower(gsub("_RMSE$", "", names(.))))
 #  
-#  stacked_rmse <- gather(rmses)
+#  # By removing the `splits` column, `rmses` is a basic data frame instead of
+#  # an `rset` object.
+#  stacked_rmse <- gather(rmses, key = model, value = statistic, -id, -id2)
 #  
 #  mean_rmse <- stacked_rmse %>%
 #    group_by(model) %>%
@@ -47,7 +49,7 @@ if(!eval_chunks)
 #    geom_line(stat = "density", trim = FALSE) +
 #    theme(legend.position = "top")
 
-## ----gamma-stats, eval = eval_chunks-------------------------------------
+## ----gamma-stats, eval = eval_chunks------------------------------------------
 #  gamma_model <- perf_mod(rmses, family = Gamma(), seed = 74)
 #  
 #  # Get the posterior distributions of the mean parameters:
@@ -55,16 +57,16 @@ if(!eval_chunks)
 #  gamma_mean <- summary(gamma_post)
 #  gamma_mean
 
-## ----gamma, eval = eval_chunks-------------------------------------------
+## ----gamma, eval = eval_chunks------------------------------------------------
 #  ggplot(gamma_post) +
 #    geom_point(data = gamma_mean, aes(y = mean), alpha = .5) +
 #    geom_point(data = mean_rmse, aes(y = statistic),
 #               col = "red", pch = 4, cex= 3)
 
-## ----log-linear-fit, eval = eval_chunks----------------------------------
+## ----log-linear-fit, eval = eval_chunks---------------------------------------
 #  log_linear_model <- perf_mod(rmses, transform = ln_trans, seed = 74)
 
-## ----log-linear, eval = eval_chunks--------------------------------------
+## ----log-linear, eval = eval_chunks-------------------------------------------
 #  log_linear_post <- tidy(log_linear_model, seed = 3750)
 #  
 #  log_linear_mean <- summary(log_linear_post)
@@ -75,7 +77,7 @@ if(!eval_chunks)
 #    geom_point(data = mean_rmse, aes(y = statistic),
 #               col = "red", pch = 4, cex= 3)
 
-## ----linear-linear, eval = eval_chunks-----------------------------------
+## ----linear-linear, eval = eval_chunks----------------------------------------
 #  linear_model <- perf_mod(rmses, seed = 74)
 #  
 #  linear_post <- tidy(linear_model, seed = 3750)
@@ -86,7 +88,7 @@ if(!eval_chunks)
 #    geom_point(data = mean_rmse, aes(y = statistic),
 #               col = "red", pch = 4, cex= 3)
 
-## ----gamma-contrast, eval = eval_chunks----------------------------------
+## ----gamma-contrast, eval = eval_chunks---------------------------------------
 #  all_contrasts <- contrast_models(linear_model, seed = 8967)
 #  ggplot(all_contrasts, size = 1)
 #  summary(all_contrasts, size = 1)
--- inst/doc/Getting_Started.R
+++ inst/doc/Getting_Started.R
@@ -1,4 +1,4 @@
-## ----check-for-build, echo = FALSE, results='asis'-----------------------
+## ----check-for-build, echo = FALSE, results='asis'----------------------------
 eval_chunks <- as.logical(Sys.getenv("local_vignette_build", FALSE))
 # Change this via `Sys.setenv(local_vignette_build = "TRUE")`
 if(!eval_chunks) 
@@ -15,7 +15,7 @@ if(!eval_chunks)
 #  theme_set(theme_bw())
 #  options(width = 100, digits = 3)
 
-## ----roc-data, eval = eval_chunks----------------------------------------
+## ----roc-data, eval = eval_chunks---------------------------------------------
 #  library(tidyposterior)
 #  data("precise_example")
 #  
@@ -28,28 +28,29 @@ if(!eval_chunks)
 #  
 #  library(ggplot2)
 #  
-#  rocs_stacked <- gather(rocs)
+#  # The rocs object is not a real `rset` object so use basic `gather()`
+#  rocs_stacked <- gather(rocs, "model", "statistic", -id)
 #  
 #  ggplot(rocs_stacked, aes(x = model, y = statistic, group = id, col = id)) +
 #    geom_line(alpha = .75) +
 #    theme(legend.position = "none")
 
-## ----linear-model, eval = eval_chunks------------------------------------
+## ----linear-model, eval = eval_chunks-----------------------------------------
 #  roc_model <- perf_mod(rocs, seed = 2824)
 
-## ----save-mod, include = FALSE, eval = eval_chunks-----------------------
+## ----save-mod, include = FALSE, eval = eval_chunks----------------------------
 #  # Two objects are saved to be used in the man files
 #  # `build_site` runs these in `tidyposterior/docs/articles`
 #  try(save(roc_model, file = "../../inst/examples/roc_model.RData", compress = TRUE), silent = TRUE)
 
-## ----linear-model-results, eval = eval_chunks----------------------------
+## ----linear-model-results, eval = eval_chunks---------------------------------
 #  roc_model$stan
 
-## ----linear-model-post, eval = eval_chunks-------------------------------
+## ----linear-model-post, eval = eval_chunks------------------------------------
 #  roc_post <- tidy(roc_model)
 #  head(roc_post)
 
-## ----linear-model-post-plot, eval = eval_chunks--------------------------
+## ----linear-model-post-plot, eval = eval_chunks-------------------------------
 #  ggplot(roc_post) +
 #    # Add the observed data to check for consistency
 #    geom_point(
@@ -58,16 +59,16 @@ if(!eval_chunks)
 #      alpha = .5, col = "blue"
 #    )
 
-## ----glm-v-nnet, eval = eval_chunks--------------------------------------
+## ----glm-v-nnet, eval = eval_chunks-------------------------------------------
 #  glm_v_nnet <- contrast_models(roc_model, "nnet", "glm")
 #  head(glm_v_nnet)
 
-## ----save-contrast, include = FALSE, eval = eval_chunks------------------
+## ----save-contrast, include = FALSE, eval = eval_chunks-----------------------
 #  try(save(glm_v_nnet, file = "../../inst/examples/glm_v_nnet.RData", compress = TRUE), silent = TRUE)
 
-## ----glm-v-nnet-summary, eval = eval_chunks------------------------------
+## ----glm-v-nnet-summary, eval = eval_chunks-----------------------------------
 #  summary(glm_v_nnet, size = 0.02)
 
-## ----glm-v-nnet-plot, eval = eval_chunks---------------------------------
+## ----glm-v-nnet-plot, eval = eval_chunks--------------------------------------
 #  ggplot(glm_v_nnet, size = 0.02)
 
